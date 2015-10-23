9e321a2fc6cb85fbd35188ba10a97a54a8df82eb
HuttonR <hutton.rounds@thebronson.com>
2015-10-23 01:42:53 -0700

    Bumped version.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'huttonr:bootstrap4',
   summary: 'Modular, customizable Bootstrap 4.',
-  version: '4.0.0_2',
+  version: '4.0.0_4',
   git: 'https://github.com/huttonr/bootstrap4',
   documentation: 'README.md'
 });
