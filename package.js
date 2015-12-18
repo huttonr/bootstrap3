08423b57e9933c5561af095026fd89686de3cc99
HuttonR <hutton.rounds@thebronson.com>
2015-12-18 12:56:24 -0800

    Version bump.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'huttonr:bootstrap3',
   summary: 'Modular, customizable Bootstrap 3.',
-  version: '3.3.5_9',
+  version: '3.3.5_10',
   git: 'https://github.com/huttonr/bootstrap3',
   documentation: 'README.md'
 });
