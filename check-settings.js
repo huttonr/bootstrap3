606bb81973920e1c79e228a5083e2d38bef509ab
HuttonR <hutton.rounds@thebronson.com>
2016-01-14 23:52:07 -0800

    Fixed check-settings.js for IE8.

--- check-settings.js
+++ check-settings.js
@@ -1,9 +1,13 @@
 // Displays warning if no bootstrap settings json file is present
 if (Meteor.isClient) {
   Meteor.startup(function () {
-    if (typeof _bootstrapSettingsFileLoaded === 'undefined' || !_bootstrapSettingsFileLoaded)
+    if (typeof _bootstrapSettingsFileLoaded === 'undefined' || !_bootstrapSettingsFileLoaded) {
       console.warn("Bootstrap disabled. Create a file named 'bootstrap-settings.json' to enable.");
-    else
-      delete _bootstrapSettingsFileLoaded;
+    } else {
+      try {
+        _bootstrapSettingsFileLoaded = undefined;
+        delete _bootstrapSettingsFileLoaded;
+      } catch (e) {}
+    }
   });
 }
