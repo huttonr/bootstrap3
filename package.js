1dcd451dba47b7a5845e930363458c30644bdc6a
HuttonR <hutton.rounds@thebronson.com>
2016-01-14 23:48:27 -0800

    Migrated package to Meteor v1.2.1 and removed the version constraint on the ecmascript package.

--- package.js
+++ package.js
@@ -2,17 +2,17 @@
 Package.describe({
   name: 'huttonr:bootstrap3',
   summary: 'Modular, customizable Bootstrap 3.',
-  version: '3.3.6',
+  version: '3.3.6_2',
   git: 'https://github.com/huttonr/bootstrap3',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.0.2');
+  api.versionsFrom('1.2.1');
+
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('jquery');
   api.use('huttonr:bootstrap3-assets@3.3.6');
-  api.use('less');
+  api.use('less'); // Currently necessary, see https://github.com/huttonr/bootstrap3/issues/2
 
   api.addFiles('check-settings.js', 'client');
 });
@@ -20,7 +20,7 @@ Package.onUse(function(api) {
 Package.registerBuildPlugin({
   name: 'build-bootstrap3',
   use: [
-    'ecmascript@0.1.6',
+    'ecmascript',
     'huttonr:bootstrap3-assets@3.3.6'
   ],
   sources: [
