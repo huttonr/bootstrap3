dec6a4cdf79efbf66917766f921f8d136c900876
HuttonR <hutton.rounds@thebronson.com>
2016-03-04 01:52:07 -0800

    Bump version and update less npm version.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'huttonr:bootstrap3',
   summary: 'Modular, customizable Bootstrap 3.',
-  version: '3.3.6_6',
+  version: '3.3.6_8',
   git: 'https://github.com/huttonr/bootstrap3',
   documentation: 'README.md'
 });
@@ -11,9 +11,9 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
 
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('huttonr:bootstrap3-assets@3.3.6_2');
+  api.use('huttonr:bootstrap3-assets@3.3.6_3');
   api.use('less'); // Currently necessary, see https://github.com/huttonr/bootstrap3/issues/2
-  api.use('ecmascript'); // Currently necessary
+  api.use('ecmascript');
 
   api.addFiles('check-settings.js', 'client');
 });
@@ -22,12 +22,12 @@ Package.registerBuildPlugin({
   name: 'build-bootstrap3',
   use: [
     'ecmascript@0.1.6',
-    'huttonr:bootstrap3-assets@3.3.6_2'
+    'huttonr:bootstrap3-assets@3.3.6_3'
   ],
   sources: [
     'plugin/bootstrap3.js'
   ],
   npmDependencies: {
-    'less': '2.5.3'
+    'less': '2.6.0'
   }
 });
